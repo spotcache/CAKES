10c1b71cdfc7e67ff7f765c3f8e92b1250a4cad7
spotcache <157260218+spotcache@users.noreply.github.com>
2024-10-19 20:33:54 +0530

    Update FileUploadRepository.java

--- src/main/java/com/CAKES/cakes/repository/FileUploadRepository.java
+++ src/main/java/com/CAKES/cakes/repository/FileUploadRepository.java
@@ -6,5 +6,5 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import java.util.List;
 
 public interface FileUploadRepository extends JpaRepository<FileUpload, Long> {
-    List<FileUpload> findByUserId(Long userId);
+    List<FileUpload> findByUser_Id(Long userId);
 }
