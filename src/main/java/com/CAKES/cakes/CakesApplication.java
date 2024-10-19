81269ce922d143369e9c33ad1538c60c11983c73
spotcache <157260218+spotcache@users.noreply.github.com>
2024-10-19 20:26:55 +0530

    Update CakesApplication.java

--- src/main/java/com/CAKES/cakes/CakesApplication.java
+++ src/main/java/com/CAKES/cakes/CakesApplication.java
@@ -1,11 +1,11 @@
 package com.example.cakes;
 
-import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import static org.springframework.boot.SpringApplication.run;
 
 @SpringBootApplication
 public class CakesApplication {
     public static void main(String[] args) {
-        SpringApplication.run(CakesApplication.class, args);
+        run(CakesApplication.class, args);
     }
 }
