f8253865692114832d2a53036dde089914db5e1d
spotcache <157260218+spotcache@users.noreply.github.com>
2024-10-19 20:28:01 +0530

    Update SecurityConfig.java

--- src/main/java/com/CAKES/cakes/config/SecurityConfig.java
+++ src/main/java/com/CAKES/cakes/config/SecurityConfig.java
@@ -3,7 +3,6 @@ package com.example.cakes.config;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 
@@ -18,17 +17,18 @@ public class SecurityConfig {
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         http
-            .authorizeRequests()
-            .antMatchers("/register", "/login").permitAll()
-            .anyRequest().authenticated()
-            .and()
-            .formLogin()
-            .loginPage("/login")
-            .defaultSuccessUrl("/upload", true)
-            .and()
-            .logout()
-            .logoutSuccessUrl("/login")
-            .permitAll();
+            .authorizeRequests(auth -> auth
+                .antMatchers("/register", "/login").permitAll()
+                .anyRequest().authenticated()
+            )
+            .formLogin(login -> login
+                .loginPage("/login")
+                .defaultSuccessUrl("/upload", true)
+            )
+            .logout(logout -> logout
+                .logoutSuccessUrl("/login")
+                .permitAll()
+            );
         return http.build();
     }
 }
