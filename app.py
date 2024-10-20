6e077992fd334194d734f558f8d0a21856a2af3d
spotcache <spotcache@gmail.com>
2024-10-20 07:21:53 +0530

    comment changes

--- app.py
+++ app.py
@@ -17,8 +17,8 @@ login_manager.login_view = 'login'
 # AWS S3 configuration
 s3_client = boto3.client(
     's3',
-    aws_access_key_id='YOUR_AWS_ACCESS_KEY',
-    aws_secret_access_key='YOUR_AWS_SECRET_KEY'
+    aws_access_key_id='AWS_ACCESS_KEY',
+    aws_secret_access_key='AWS_SECRET_KEY'
 )
 
 from models import User, FileUpload
