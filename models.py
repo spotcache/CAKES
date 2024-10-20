a2cfac4237c056912ed0f390c241ff97c12a9e34
spotcache <157260218+spotcache@users.noreply.github.com>
2024-10-20 07:16:20 +0530

    Create models.py

---
+++ models.py
@@ -0,0 +1,16 @@
+from app import db
+from flask_login import UserMixin
+from datetime import datetime
+
+class User(UserMixin, db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(150), unique=True, nullable=False)
+    password = db.Column(db.String(150), nullable=False)
+
+class FileUpload(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    filename = db.Column(db.String(150), nullable=False)
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
+    upload_time = db.Column(db.DateTime, default=datetime.utcnow)
+    last_checkin = db.Column(db.DateTime)
+    timer_duration = db.Column(db.Integer)  # in hours
